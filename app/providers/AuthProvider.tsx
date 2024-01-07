5af656460cffe0038ce3a752abc2b052609d91b2
vherasymenko <manqarium@gmail.com>
2024-01-07 13:04:57 +0200

    [Feature] create Auth

---
+++ app/providers/AuthProvider.tsx
@@ -0,0 +1,50 @@
+import React, {
+  createContext,
+  Dispatch,
+  FC,
+  PropsWithChildren,
+  SetStateAction,
+  useEffect,
+  useState,
+} from 'react'
+import { User } from '@/types/user.interface'
+import * as Splash from 'expo-splash-screen'
+
+export type TypesUserState = User | null
+
+interface Context {
+  user: TypesUserState
+  setUser: Dispatch<SetStateAction<TypesUserState>>
+}
+
+export const AuthContext = createContext<Context>({} as Context)
+
+Splash.preventAutoHideAsync()
+
+const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [user, setUser] = useState<TypesUserState>({
+    _id: '1',
+    email: 'email@email.email',
+  })
+
+  useEffect(() => {
+    let isMounted = true
+
+    const getUserFromStorage = async () => {
+      if (isMounted) {
+      }
+
+      await Splash.hideAsync()
+    }
+
+    getUserFromStorage()
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
+  return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>
+}
+
+export default AuthProvider
